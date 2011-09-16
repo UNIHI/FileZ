7eb5bb9b7ce5ac8b6e809080070c4833b6dcfff2
pzystorm <richterk@uni-hildesheim.de>
2011-09-16 14:21:45 +0200

    Space inserted

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -35,7 +35,7 @@ class App_Controller_Main extends Fz_Controller {
 
         $progressMonitor = fz_config_get ('app', 'progress_monitor');
         $progressMonitor = new $progressMonitor ();
-        $folders = implode(',', Fz_Db::getTable('File')->getFolders($user));
+        $folders = implode(', ', Fz_Db::getTable('File')->getFolders($user));
         
         set ('upload_id'        , md5 (uniqid (mt_rand (), true)));
         set ('start_from'       , Zend_Date::now ()->get (Zend_Date::DATE_SHORT));
