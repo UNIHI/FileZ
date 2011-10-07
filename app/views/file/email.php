b8dd91f27092af23887e98170526f63b3314a90b
pzystorm <richterk@uni-hildesheim.de>
2011-10-07 12:52:25 +0200

    Replaced french sentence

--- app/views/file/email.php
+++ app/views/file/email.php
@@ -1,4 +1,4 @@
 
-<h2>Envoyer le fichier par email <span class="filename">(<?php echo h($file->file_name) ?>)</span></h2>
+<h2><?php __('Send this link via mail:') ?> <span class="filename">(<?php echo h($file->file_name) ?>)</span></h2>
 
 <?php echo partial ('file/_mailForm.php', array ('file' => $file)) ?>
